15f6fd0360baa93ac6186c0219ff862a017110a2
Charlie <yatescharles9@gmail.com>
2025-09-21 22:35:01 -0700

    Initial commit

---
+++ qwen_finetuning_script.py
@@ -0,0 +1,98 @@
+
+from unsloth import FastLanguageModel
+import dotenv
+dotenv.load_dotenv()
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/Qwen3-4B-Instruct-2507",
+    max_seq_length = 2048,   # Context length - can be longer, but uses more memory
+    load_in_4bit = True,     # 4bit uses much less memory
+    load_in_8bit = False,    # A bit more accurate, uses 2x memory
+    full_finetuning = False, # We have full finetuning now!
+    # token = "hf_...",      # use one if using gated models
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 32,           # Choose any number > 0! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 32,  # Best to choose alpha = rank or rank*2
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    use_rslora = False,   # We support rank stabilized LoRA
+    loftq_config = None,  # And LoftQ
+)
+
+from datasets import load_dataset
+dataset = load_dataset("Bossologist/general_Qwen3_ft_dataset", split = "train")
+dataset = dataset.shuffle(seed=42).select(range(100000))
+
+from trl import SFTTrainer, SFTConfig
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    eval_dataset = None, # Can set up evaluation!
+    args = SFTConfig(
+        dataset_text_field = "text",
+        dataset_num_proc=1,
+        per_device_train_batch_size = 4,
+        gradient_accumulation_steps = 4, # Use GA to mimic batch size!
+        warmup_steps = 5,
+        num_train_epochs = 3, # Set this for 1 full training run.
+        # max_steps = 30,
+        learning_rate = 2e-5, # Reduce to 2e-5 for long training runs
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        report_to = "none", # Use this for WandB etc
+    ),
+)
+
+trainer.train()
+
+messages = [
+    {"role" : "user", "content" : "Solve (x + 2)^2 = 0."}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize = False,
+    add_generation_prompt = True, # Must add for generation
+    enable_thinking = False, # Disable thinking
+)
+
+from transformers import TextStreamer
+_ = model.generate(
+    **tokenizer(text, return_tensors = "pt").to("cuda"),
+    max_new_tokens = 256, # Increase for longer outputs!
+    temperature = 0.7, top_p = 0.8, top_k = 20, # For non thinking
+    streamer = TextStreamer(tokenizer, skip_prompt = True),
+)
+
+from huggingface_hub import create_repo
+import os
+
+token = os.getenv("HF_TOKEN")
+repo_id = "Bossologist/Qwen3-4B-Instruct-2507_general_ft_lora"
+create_repo(
+    repo_id,
+    exist_ok=True,
+    token=token
+)
+
+model.push_to_hub(repo_id, token = token) # Online saving
+tokenizer.push_to_hub(repo_id, token = token) # Online saving
+
+from unsloth import FastLanguageModel
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = repo_id, # YOUR MODEL YOU USED FOR TRAINING
+    max_seq_length = 2048,
+    load_in_4bit = True,
+)
+
+model.push_to_hub_gguf(repo_id, tokenizer, quantization_method = "q4_k_m", token = token)
