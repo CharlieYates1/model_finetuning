9be06a3adcc38b499a85c4f4857febeff4681a62
Charlie <yatescharles9@gmail.com>
2025-09-25 11:34:35 -0700

    using general ft model

--- qwen_finetuning_script.py
+++ qwen_finetuning_script.py
@@ -3,7 +3,7 @@ from unsloth import FastLanguageModel
 import dotenv
 dotenv.load_dotenv()
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "unsloth/Qwen3-4B-Instruct-2507",
+    model_name = "Bossologist/Qwen3-4B-Instruct-2507_general_ft_merged",
     max_seq_length = 16384,   # Context length - can be longer, but uses more memory
     load_in_4bit = True,     # 4bit uses much less memory
     load_in_8bit = False,    # A bit more accurate, uses 2x memory
